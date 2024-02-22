a83503e04d6e3867a86a6e39e071234356400e9e
Kartik <kartikteja343@gmail.com>
2024-02-22 18:39:51 +0530

    hello

---
+++ HTTPServer/SimpleHTTPServer.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.net.*;
+
+public class SimpleHTTPServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        System.out.println("Server is listening on port 8080...");
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+
+            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+            String line = reader.readLine();
+            while (!line.isEmpty()) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+
+        }
+    }
+}
